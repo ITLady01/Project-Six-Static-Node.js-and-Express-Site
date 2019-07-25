1f211220f547646cc03e229d01315eb001f2b17b
nwokiwu <51241053+nwokiwu@users.noreply.github.com>
2019-07-25 12:18:12 -0400

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,13 +3,20 @@ $(document).foundation()
 projects = {
     [{
             "title": "Random Generator",
-          "url": "http://github.com",
+            "url": // https://itlady01.github.io/Project-Two-List-Pagination-and-filtering/,
          
         },
+    
         {
             "title": " Project Two Pagination and Filter ",
             "url": " https://itlady01.github.io/Project-Two-List-Pagination-and-filtering/"
         },
+        {
+            "title": "Fiat",
+            "url": ["500", "Panda"]
+        }
+
+
         {
             "title": "Fiat",
             "url": ["500", "Panda"]
