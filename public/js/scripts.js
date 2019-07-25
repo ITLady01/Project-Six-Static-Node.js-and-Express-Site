bab3c39f288bde7251eb6a29936d3a7d05d35274
nwokiwu <51241053+nwokiwu@users.noreply.github.com>
2019-07-25 11:39:32 -0400

    Updated files for Project
    Project six draft updates

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,2 +1,18 @@
 $(document).foundation()
 
+projects = {
+    [{
+            "title": "Random Generator",
+          "url": "http://github.com",
+         
+        },
+        {
+            "title": " Project Two Pagination and Filter ",
+            "url": " https://itlady01.github.io/Project-Two-List-Pagination-and-filtering/"
+        },
+        {
+            "title": "Fiat",
+            "url": ["500", "Panda"]
+        }
+    ]
+}
