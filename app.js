49c537e6c677638f96fcb04a9e8d7547bbed0555
nwokiwu <51241053+nwokiwu@users.noreply.github.com>
2019-07-29 10:14:50 -0400

    updates

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.get('/about', (req, res) => {
 // Creates a new 404 status error
 app.use((req, res, next) => {
   const err = new Error("Not Found");
-  err.status = 404;
+  err.status = 404;s
   next(err);
 });
 
